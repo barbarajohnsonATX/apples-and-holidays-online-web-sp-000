6e137beefe6b351e9e7cceee8da55c5387834ea8
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-07 15:25:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |day, supplies|
       day_string = day.to_s.capitalize 
       day_array << day_string
-      binding.pry 
+      #binding.pry 
       day_string = day_array.join(" ")
        day_string.gsub!("_", " ")
       puts "#{season.capitalize}:"
