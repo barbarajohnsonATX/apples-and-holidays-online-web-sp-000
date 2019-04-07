28c1ec08d396de3ff6750a12b671c8727c5ba3d1
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-07 16:49:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,13 +81,13 @@ def all_supplies_in_holidays(holiday_hash)
     
     
     holiday.each do |day, supplies|
-        day_string = day.to_s.split("_").join 
+        day_string = day.to_s.split("_").join(" ")
         day_temp_array << day_string
+    binding.pry 
 
      end 
     
     
-    binding.pry 
   end 
 
 end
